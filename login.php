5a92f8513fcd807bd6c66e31a979700c759f5e72
saraSL <sara.lundqviist@gmail.com>
2020-02-03 15:47:54 +0100

    changed names of login and login2

--- login.php
+++ login.php
@@ -1,34 +1,21 @@
-<?php
-// session_start();
-// if (isset($_POST['submit']))
-//{
-//    if(($_POST['captchacode']) == $_SESSION['captchacode'])
-//        { // Do process the other submitted form data 
-//        }
-//    else
-//        { $_SESSION['error']= "The Captcha code is wrong. Try again.";
-//        // redirect to another page or to the form page again
-//        header("index.php"); // this is a link back to our regform.php
-//        exit();
-//    }
-//}
-?> 
-<?php
-include 
-$email = $_POST['useremail'];
-$password = $_POST['userpassword'];
-$confirmpassword = $_POST['confirmpasswork'];
-$email = mysql_real_escape_string($email);
-$password = mysql_real_escape_string($password);
-$confirmpassword = mysql_real_escape_string($confirmpassword);
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-if (filter_val($email, FILTER_VALIDATE_EMAIL))
-{
-    
-}
-else
-{
-    header('Location: http://f2fd.com/f2fd_main');
-}
-?>
+<!DOCTYPE html>
+<html>
+<body>
+<form action="???.php" method="POST">
+
+<div>
+<input type="text" name="email"><br>
+<input type="text" name="password"><br>
+
+<div>
+<button type="button">Cancel</button>
+<button type="submit">Login</button>
+</div>
+
+<input type="submit" value="Register">
+
+</div>
+</form>
+</body>
+</html>
 
