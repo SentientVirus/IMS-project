912905939c073b28aad2263ab5f9511ca1b62e2e
Marina Mota <marina.mota.merlo@gmail.com>
2020-02-08 23:32:39 +0100

    Welcome page updated

--- index.php
+++ index.php
@@ -30,8 +30,9 @@
             <br>
             <!-- <div class = "table" style = "margin: auto; border-style: outset;
             width: 500px; height:150px; padding:30px;">-->
-            <div id = "block_container">
+            <div id = "block_container" style = "margin-top: 50px;">
             <div id = "bloc1" class = "table" style = "margin-right: 10px; width: 500px;">
+              <h1>About</h1>
               <p>We provide questionnaires based on GWAS studies from UK Biobank.
               By taking this questionnaires, which are about some of your
               manifested traits (phenotype), you will get some insight about
@@ -42,25 +43,34 @@
               <img src="flecha.gif" alt="Arrow"
                   style="width: 150px; height: 50px; margin-bottom: 20px;"/>
               <img src="eye.png" alt="Depression"
-                  style="width: 100px; height: 100px;"/></div>
+                  style="width: 100px; height: 100px;"/>
+              <p>
+            </div>
             <div id = "bloc2" class = "table" style = "margin-left: 10px; width: 500px;">
+              <h1>Why take our test</h1>
               <p>add content</p></div>
             </div>
             <br>
-            <div class = "typewriter" style = "margin: auto; border-radius: 10px; width: 500px; height:120px; padding:30px;">
-            <h1 style = "text-align: center;">What do you want to do?</h1>
-            <hr style = "border: 0; height: 1px; background: #333;
-            background-image: linear-gradient(to right, #ccc, #333, #ccc);">
-            <br>
-            <br>
-            <a class = "btn btn1" href='questionnaire.php'
+              <div class = "typewriter" style = "margin: auto; border-radius: 10px; width: 500px; height:120px; padding:30px;">
+                <h1 style = "text-align: center;">What do you want to do?</h1>
+                <hr style = "border: 0; height: 1px; background: #333;
+                background-image: linear-gradient(to right, #ccc, #333, #ccc);">
+                <br>
+                <br>
+            <!--<a class = "btn btn1" href='questionnaire.php'
             style = "text-decoration: none; margin-left:70px;
             font-family: 'Tahoma', sans-serif; font-size: 14px;">
-              Take the test</a>
-            <a href='login.php' class = "btn btn1" href='login.php'
-            style = "text-decoration: none; margin-left: 50px; margin-right:50px;
-            font-family: 'Tahoma', sans-serif; font-size: 14px;">To log in page</a>
-            </div>
+              Take the test</a>-->
+              <select autofocus name = "illness" onchange="location = this.value;" class = "btn btn1"; style = "text-decoration: none; margin-left:70px;
+              margin-top: -20px; height: 56px; font-family: 'Tahoma', sans-serif; font-size: 14px;">
+              <option value = "" selected = "selected">Choose test</option>
+              <option value= "depression.php">Depression</option>
+              <option value= "illness2.php">Illness2</option></select>
+
+              <a href='login.php' class = "btn btn1" href='login.php'
+              style = "text-decoration: none; margin-left: 50px; margin-right:50px;
+              font-family: 'Tahoma', sans-serif; font-size: 14px;">To log in page</a>
+              </div>
             <br>
             <div id = "block_container">
             <div id = "bloc1" class = "table" style = "margin-right: 10px; width: 500px;">
