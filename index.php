57fcd5e71b2bb20b895a34ae9186d4a5e9939dea
Marina Mota <marina.mota.merlo@gmail.com>
2020-02-09 19:49:22 +0100

    More images and last version of Welcome page

--- index.php
+++ index.php
@@ -16,6 +16,24 @@
             <img src="f2fd_logo.png" alt="A generic logo"
                  style="width: 150px;height: 150px" id="logo">
         </div> -->
+        <div id = "overlayer" onclick="off()"></div>
+        <div class = "typewriter" style = "margin: auto; border-radius: 10px;
+        width: 500px; padding:30px; margin-top:20px;">
+          <h1 style = "text-align: center;">What do you want to do?</h1>
+          <hr style = "border: 0; height: 1px; background: #333;
+          background-image: linear-gradient(to right, #ccc, #333, #ccc);">
+          <br>
+          <br>
+        <select autofocus name = "illness" onchange="location = this.value;" class = "btn btn1"; style = "text-decoration: none; margin-left:70px;
+        margin-top: -20px; height: 56px; font-family: 'Tahoma', sans-serif; font-size: 14px;">
+        <option value = "" selected = "selected">Choose test</option>
+        <option value= "depression.php">Depression</option>
+        <option value= "illness2.php">Illness2</option></select>
+
+        <a href='login.php' class = "btn btn1" href='login.php'
+        style = "text-decoration: none; margin-left: 50px; margin-right:50px;
+        font-family: 'Tahoma', sans-serif; font-size: 14px;">To log in page</a>
+      </div>
         <div id="main" style = "margin:auto;">
             <?php
                 session_start();
@@ -28,55 +46,91 @@
                 }
             ?>
             <br>
+            <div class="navbar" style = "margin-top:-18px;">
+              <a class="active" href="index.php"><i class="fa fa-fw fa-home"></i> Home</a>
+              <div class="dropdown">
+                <button class="dropbtn">Dropdown
+                  <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                  <a href="depression.php">Depression</a>
+                  <a href="#">Illness2</a></div></div>
+              <a href="login.php"> Login</a>
+              <a href="#"> Profile</a>
+            </div>
             <!-- <div class = "table" style = "margin: auto; border-style: outset;
             width: 500px; height:150px; padding:30px;">-->
             <div id = "block_container" style = "margin-top: 50px;">
-            <div id = "bloc1" class = "table" style = "margin-right: 10px; width: 500px;">
-              <h1>About</h1>
-              <p>We provide questionnaires based on GWAS studies from UK Biobank.
-              By taking this questionnaires, which are about some of your
-              manifested traits (phenotype), you will get some insight about
-              whether you are likely to develop an illness (which is also a
-              trait, hence the name phenotype-to-phenotype).</p>
-              <img src="tacita.png" alt="Tea cup"
-                 style="width: 120px; height: 100px; margin-left: 50px;"/>
-              <img src="flecha.gif" alt="Arrow"
-                  style="width: 150px; height: 50px; margin-bottom: 20px;"/>
-              <img src="eye.png" alt="Depression"
-                  style="width: 100px; height: 100px;"/>
-              <p>
+            <div id = "bloc1" class = "container" style = "margin-right: 10px; width: 600px;">
+              <img src="overlay.png" alt="Avatar" class="image">
+              <div class="overlay" style = "margin-top: 60px;">
+                <h1 style = "margin-bottom:-20px; margin-left:0px; width: 570px;">About</h1>
+                <div class = "table">
+                  <div>
+                  <p>We provide questionnaires based on GWAS studies from UK Biobank.
+                    By taking this questionnaires, which are about some of your
+                    manifested traits (phenotype), you will get some insight about
+                    whether you are likely to develop an illness (which is also a
+                    trait, hence the name phenotype-to-phenotype).</p>
+                    <img src="tacita.png" alt="Tea cup"
+                       style="width: 120px; height: 100px; margin-left: 100px;"/>
+                    <img src="flecha.gif" alt="Arrow"
+                        style="width: 150px; height: 50px; margin-bottom: 20px;"/>
+                    <img src="eye.png" alt="Depression"
+                        style="width: 100px; height: 100px;"/>
+                  <p>For example, there is a correlation between tea intake
+                    and depression. This means that, if you drink a lot of tea,
+                    you are more likely to have depression.</p>
+              <p>Based on this and other traits, our website assesses
+                whether you are likely or not to have depression or other conditions
+                according to your answers, but keep in mind that there are more
+                factors to take into account and a positive result does <b>not</b> mean
+                that you will develop the illness.</p>
+              </div>
+              </div>
+              </div>
             </div>
-            <div id = "bloc2" class = "table" style = "margin-left: 10px; width: 500px;">
-              <h1>Why take our test</h1>
-              <p>add content</p></div>
+            <div id = "bloc2" class = "container" style = "margin-left: 10px; width: 600px;">
+              <img src="overlay2.png" alt="Avatar" class="image">
+              <div class="overlay" style = "margin-top: 60px;">
+                <h1 style = "margin-bottom:-20px; margin-left:0px; width: 570px;">
+                Why take our test</h1>
+                <div class = "table">
+                  <div>
+                  <p>We provide questionnaires based on GWAS studies from UK Biobank.
+                    By taking this questionnaires, which are about some of your
+                    manifested traits (phenotype), you will get some insight about
+                    whether you are likely to develop an illness (which is also a
+                    trait, hence the name phenotype-to-phenotype).</p>
+                    <img src="tacita.png" alt="Tea cup"
+                       style="width: 120px; height: 100px; margin-left: 100px;"/>
+                    <img src="flecha.gif" alt="Arrow"
+                        style="width: 150px; height: 50px; margin-bottom: 20px;"/>
+                    <img src="eye.png" alt="Depression"
+                        style="width: 100px; height: 100px;"/>
+                  <p>For example, there is a correlation between tea intake
+                    and depression. This means that, if you drink a lot of tea,
+                    you are more likely to have depression.</p>
+              <p>Based on this and other traits, our website assesses
+                whether you are likely or not to have depression or other conditions
+                according to your answers, but keep in mind that there are more
+                factors to take into account and a positive result does <b>not</b> mean
+                that you will develop the illness.</p>
+              </div>
+              </div>
+              </div>
+            </div>
             </div>
             <br>
-              <div class = "typewriter" style = "margin: auto; border-radius: 10px; width: 500px; height:120px; padding:30px;">
-                <h1 style = "text-align: center;">What do you want to do?</h1>
-                <hr style = "border: 0; height: 1px; background: #333;
-                background-image: linear-gradient(to right, #ccc, #333, #ccc);">
-                <br>
-                <br>
-            <!--<a class = "btn btn1" href='questionnaire.php'
-            style = "text-decoration: none; margin-left:70px;
-            font-family: 'Tahoma', sans-serif; font-size: 14px;">
-              Take the test</a>-->
-              <select autofocus name = "illness" onchange="location = this.value;" class = "btn btn1"; style = "text-decoration: none; margin-left:70px;
-              margin-top: -20px; height: 56px; font-family: 'Tahoma', sans-serif; font-size: 14px;">
-              <option value = "" selected = "selected">Choose test</option>
-              <option value= "depression.php">Depression</option>
-              <option value= "illness2.php">Illness2</option></select>
-
-              <a href='login.php' class = "btn btn1" href='login.php'
-              style = "text-decoration: none; margin-left: 50px; margin-right:50px;
-              font-family: 'Tahoma', sans-serif; font-size: 14px;">To log in page</a>
-              </div>
+            </div>
             <br>
             <div id = "block_container">
             <div id = "bloc1" class = "table" style = "margin-right: 10px; width: 500px;">
-              <p>add content</p></div>
+              <p><a href= "terms_privacy.php">Terms & Privacy</a></p></div>
             <div id = "bloc2" class = "table" style = "margin-left: 10px; width: 500px;">
-              <p>To be completed</p></div>
+              <p>Data from <a href= "https://www.ukbiobank.ac.uk/">
+                UK Biobank</a> and cover images from <a href = "https://pixabay.com/">
+                  Pixabay</a></p></div>
             </div>
             <!--<form id="form">
                 <div>
@@ -100,6 +154,20 @@
                 </div>
             </form>
         </div></aside>
+        <script>
+        function on() {
+          document.getElementById("overlayer").style.display = "block";
+        }
+
+        function off() {
+          document.getElementById("overlayer").style.display = "none";
+          var x = document.getElementsByClassName("typewriter");
+          var i;
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+          }
+        }
+      </script>
     </body>
     <footer>
 
