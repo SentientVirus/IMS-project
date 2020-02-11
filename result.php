ecf4d97b3dfa8d27e04fe26c65c8de91e9169b2f
Yiwen Chen <brandonchen2932@outlook.com>
2020-02-11 20:58:11 +0100

    made the result.php running. Check it out!

---
+++ result.php
@@ -0,0 +1,72 @@
+<?php
+include("connectDB.php");
+//1. Receive the result from questionnaire.php
+//1. Calculate the result from the questionnaire.php
+//2. HTML page showing the results
+$result = $link -> query("select disease_name, Traits.id, question, rg
+from Diseases, Traits, Correlations
+where Diseases.id = disease_id and Traits.id = trait_id and disease_name = 'Depression';");
+$length = mysqli_num_rows($result);
+$max_score = 0;
+foreach ($result as $row) {
+  $max_score += $row["rg"];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Your Result!</title>
+  </head>
+
+  <body>
+    <h2>Hi there!</h2>
+    <?php
+    $score = 0;
+    for($i = 1; $i < $length + 1; $i++) {
+      if ($_POST["Q".$i] == "Yes"){
+        $query4RG = mysqli_query($link,"select rg from Correlations
+        where trait_id = {$i} and disease_id = 1;");
+        foreach ($query4RG as $row) {
+          $score += $row["rg"];
+        }
+      }
+    }
+    echo "Your absolute score is:".$score;
+    $rel_score = $score*100/$max_score;
+    if ($rel_score <= 50) {
+      echo "Your mental health is good as FUCK!!! Enjoy your life and rock on!";
+    } else {
+      $compare_to_50 = round(($rel_score-50)*2, 2);
+      echo "<br><hr><br>You are {$compare_to_50}% more likely to develop
+      depression in the rest of your life than normal people. Congratulations!";
+
+    }
+
+
+
+
+
+    /*
+    $score = 0;
+    for($i = 1; $i < $length + 1; $i++) {
+      if ($_POST["Q".$i] == "Yes"){
+        $rg = mysqli_query($link,"select rg from Correlations
+        where disease_id = 1 and trait_id = ".$i." ;");
+        $score += $rg;
+
+    }
+    echo $score; */
+
+    include("connectDB.php");
+
+    ?>
+
+
+
+
+  </body>
+
+</html>
