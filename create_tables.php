5afd0d1cc9600afc2c0b52bb90426419f63bc03a
saraSL <sara.lundqviist@gmail.com>
2020-02-12 15:38:39 +0100

    removed unique for username

--- create_tables.php
+++ create_tables.php
@@ -16,7 +16,7 @@ if(mysqli_query($link, $sql)) {
 // sql to create table
 $sql = "CREATE TABLE Users (
 id INT AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(50) NOT NULL UNIQUE,
+username VARCHAR(50) NOT NULL,
 email VARCHAR(50) NOT NULL UNIQUE,
 password VARCHAR(255) NOT NULL,
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
