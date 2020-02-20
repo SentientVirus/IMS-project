1c630884b1ffd2cf602902dd57e6f491f74faf59
Lil-Gepi <brandonchen2932@outlook.com>
2020-02-20 18:46:10 +0100

    Built the basic functions of admin

--- admin.php
+++ admin.php
@@ -5,43 +5,49 @@
     <meta charset="utf-8">
     <title>Admin_f2fd</title>
   </head>
+
   <body>
     <h1>Hi Admin, enjoy your power</h1>
-<!--1. make three uttons with three tables to alter: Diseases, Traits, Correlations.
-    2. make them invisible and only visible if the user has chosen them
-    3. post the data which the admin inserted to the form and ost to the same page to insert them
-  -->
+    <!--1. make three uttons with three tables to alter: Diseases, Traits, Correlations.
+      2. make them invisible and only visible if the user has chosen them
+      3. post the data which the admin inserted to the form and ost to the same page to insert them
+    -->
   <div class="">
 
-    <form action="insert_data_admin.php" target="_self">
+    <form action="insert_data_admin.php" target="_self" method="post">
+
       Please enter the disease's name:<br>
       <input type="text" id="D_name" name="D_name"><br><br>
+
       Please enter the disease's description:<br>
       <input type="text" id="D_desc" name="D_desc"><br><br>
+
       <input type="submit" value="Submit">
     </form>
   </div>
 
-<br>
-<hr>
-<br>
+  <br>
+  <hr>
+  <br>
 
-<div class="">
-  <form action="insert_data_admin.php" target="_self">
-    Please the question sentence:<br>
-    <input type="text" id="T_name" name="T_name"><br>
-    <input type="submit" value="Submit">
-  </form>
+  <div class="">
+    <form action="insert_data_admin.php" target="_self">
 
-</div>
+      Please enter the question sentence:<br>
+      <input type="text" id="T_name" name="T_name"><br><br>
 
-  <form action="insert_data_admin.php" target="_self">
-    <label>Insert Correlations:</label><br>
-    <input type="text" id="D_name" name="D_name"><br>
-    <input type="submit" value="Submit">
-  </form>
+      Please choose the disease's name:<br>
+      <input type="text" id="C_D_name" name="C_D_name"><br><br>
+
+      Please enter the correlation between those two (floating value like 0.45512):<br>
+      <input type="text" id="C_rg" name="C_rg"><br><br>
+
+      <input type="submit" value="Submit">
 
 
+    </form>
+
+  </div>
 
 
 
